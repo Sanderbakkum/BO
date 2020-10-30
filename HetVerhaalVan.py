6e6941d99fbb47910209260313ef5de929675956
Sander <35122509+Sanderbakkum@users.noreply.github.com>
2020-10-30 08:56:42 +0100

    Add files via upload

--- HetVerhaalVan.py
+++ HetVerhaalVan.py
@@ -1,54 +1,72 @@
 #Dit is mijn eindopdracht voor deze periode
 #Mijn verhaal gaat over Zafer, Hij komt uit Syrie
 
+def lijn():
+	print("------------------------------------------------------------")
+
+def Tussenlijn():
+	print("")
+
+def fout():
+	print("Fout begin maar opnieuw")
+
+def goed():
+	print("Klopt dat is goed")
+
 print("Hallo ik ben Sander")
 print("Ik ga jullie het verhaal van Zafer vertellen.")
 print("Ik heb zijn verhaal gekozen omdat hij bij ons in de klas kwam vertellen over zijn ervaringen.")
 print("Het verhaal vertel ik door wat korte vraagjes te stellen over zijn leven.")
+print("Hou altijd je CAPS aan zodat je geen verkeerde antwoorden krijgt")
 
-print("")
+Tussenlijn()
+lijn()
+Tussenlijn()
 
 print("Eerste vraag:")
 print("Uit welk land komt Zafer?")
 
-print("")
+Tussenlijn()
 
 print("A: Irak")
 print("B: Syrie")
 print("C: Turkije")
 
-print("")
+Tussenlijn()
 
 Message=input()
 if Message=="B":
-	print(" ")
+	Tussenlijn()
 	print("Klopt goed gegokt")
-	print("------------------------------------------------------------")
+	lijn()
 
-	print("")
+	Tussenlijn()
 else:
-	print("")
+	Tussenlijn()
 	print("Fout geraden het was A")
-	print("------------------------------------------------------------")
-	print("")
+	lijn()
+	Tussenlijn()
 
 print("Tweede vraag:")
 print("Waar zou je meer over willen weten")
-print("")
+
+Tussenlijn()
+
 print("A: Voor vertrek")
 print("B: Reis")
 print("C: In Nederland")
 print("D: De toekomst")
 
-print("")
+Tussenlijn()
 
 Message2=input()
 if Message2=="A":
-	print("")
-	print("------------------------------------------------------------")
+# 5 Stukken Tekst
+	Tussenlijn()
+	lijn()
 	print("Ik ga je nu wat vertellen over voor het vertrek.")
 
-	print("")
+	Tussenlijn()
 
 	print("Zafer was erg populair in het gedeelte waar hij woonde hij was namelijk")
 	print("een dichter. In Syrie was waren veel mensen het niet eens met de")
@@ -57,25 +75,26 @@ if Message2=="A":
 	print("over de situatie daar. De regering was het daar niet mee eens en")
 	print("daarom werd hij ook gezocht.")
 
-	print("")
+	Tussenlijn()
 
 	print("Waarom werd hij gezocht?")
 
-	print("")
+	Tussenlijn()
 
 	print("A: Hij was het niet eens met de dictatuur")
 
 	print("B: Hij had een misdaad gepleegd.")
-	print("")
+
+	Tussenlijn()
 
 	Message3=input()
 	if Message3=="A":
 
-		print("")
-		print("Klopt")
-		print("------------------------------------------------------------")
+		Tussenlijn()
+		goed()
+		lijn()
 
-		print("")
+		Tussenlijn()
 
 		print("Zafer had veel mensen die dezelfde mening hadden over hoe")
 		print("ze niet meer in deze dictatuur wilden leven dus namen ze")
@@ -85,121 +104,388 @@ if Message2=="A":
 
 		print("Welke stad bestuurde ze?")
 
-		print("")
+		Tussenlijn()
 
 		print("A: Douma")
 		print("B: Daraa")
 		print("C: Damascus")
 
-		print("")
+		Tussenlijn()
 
 		Message4=input()
 		if Message4=="C":
 
-			print("")
-			print("Goed")
-			print("------------------------------------------------------------")
+			Tussenlijn()
+			goed()
+			lijn()
 
-			print("")
-
+			Tussenlijn()
 			print("De dictatuur in het land werdt zo erg dat ze als maar")
 			print("werden aangevallen. De bommen die bleven maar op")
 			print("Damascus geschoten en ze hadden uit eindelijk zoveel")
 			print("ervaringen met de bommen dat ze de bommen konden")
 			print("ontwijken op het geluid van de bommen.")
 
-			print("")
+			Tussenlijn()
 
 			print("Zou jij zelf al gevlucht zijn?")
 
-			print("")
+			Tussenlijn()
 
 			print("A: Ja")
 			print("B: Nee")
 
-			print("")
+			Tussenlijn()
 
 			Message5=input()
 			if Message5=="A":
 
-				print("")
+				Tussenlijn()
 
 				print("Ik zelf ook alleen dat zou niet kunnen, omdat hij")
 				print("een belangerijke taak had in die stad.")
 
-				print("")
-				print("------------------------------------------------------------")
+				Tussenlijn()
+				lijn()
 				print("Zijn gezin was wel al gevlucht naar een ander")
 				print("deel van Syrie, omdat het te gevaarlijk werd.")
 				print("Het werd nu ook te gevaarlijk voor Zafer om")
 				print("te blijven. Hij koos ervoor om te vluchten.")
-				print("Einde Verhaal: Voor Vertrek")
-
-				print("")
+				lijn()
+				print("dit was het einde van het verhaal over voor")
+				print("zijn vertrek. Wil je meer weten begin dan")
+				print("opnieuw en kies een andere richting.")
+				lijn()
 
 			else:
 
-				print("")
-				print("------------------------------------------------------------")
+				lijn()
+				Tussenlijn()
+
 				print("Ik zelf wel maar dat is wat ik zal doen. Zafer")
 				print("daarin tegen die was niet gevlucht omdat hij")
 				print("hij een belangerijke taak had in Damascus")
 
-				print("")
-
+				Tussenlijn()
+				lijn()
 				print("Zijn gezin was wel al gevlucht naar een ander")
 				print("deel van Syrie, omdat het te gevaarlijk werd.")
 				print("Het werd nu ook te gevaarlijk voor Zafer om")
 				print("te blijven. Hij koos ervoor om te vluchten.")
-				print("Einde Verhaal: Voor Vertrek")
-				print("")
-
+				lijn()
+				print("dit was het einde van het verhaal over voor")
+				print("zijn vertrek. Wil je meer weten begin dan")
+				print("opnieuw en kies een andere richting.")
+				lijn()
 		else:
 
-			print("")
-			print("Fout je moet nu opnieuw beginnen")
-			print("------------------------------------------------------------")
-			print("")
+			Tussenlijn()
+			fout()
+			lijn()
+			Tussenlijn()
 
 	else:
 
-		print("")
-		print("Fout Je zou nu weer helemaal opnieuw moeten beginnen")
-		print("------------------------------------------------------------")
+		Tussenlijn()
+		fout()
+		lijn()
 
-		print("")
-
+		Tussenlijn()
 if Message2=="B" :
-	print("")
-	print("------------------------------------------------------------")
+# 7 Stukken Tekst
+	Tussenlijn()
+	lijn()
 	print("ik ga je nu vragen stellen over de reis zodat jij meer te weten komt.")
-	print("")
+	Tussenlijn()
 	print("Zafer was erg belangerijk in de stad Damascus dus het duurde")
 	print("lang voor dat hij uiteindelijk gevlucht was. Ze vrouw")
-	print("en kinderen waren al gevlucht. Hij had 3 kinderen.")
+	print("en kinderen waren al gevlucht naar een andere deel van Syrie")
+	print(". Hij had 3 kinderen.")
 	print("Ze waren erg jong toen ze bij hun vader weg moesten.")
 
 	print("Waarom ging hij niet mee met zijn gezin")
-	print("")
+	Tussenlijn()
 	print("A: Hij was nog klaar met de stad om weg te gaan")
 	print("B: Hij wou nog niet gaan omdat hij niet weg wilden van huis")
-	print("")
+	Tussenlijn()
 
 	Message6=input()
 	if Message6=="A":
-		print("")
-		print("Klopt hij bleef langer voor de stad")
-		print("------------------------------------------------------------")
-	if Message6=="B":
-		print("")
-		print("Fout, Begin maar opnieuw.")
-		print("------------------------------------------------------------")
-if Message2=="C":
-	print("")
-	print("Ik ga je nu wat vertellen over hoe het in Nederland.")
-	print("")
+		Tussenlijn()
+		goed()
 
+		lijn()
+
+		print("Zafer zijn kinderen waren nog erg jong. Hij had zijn laatste")
+		print("nog nooit gezien.")
+		print("Uiteindelijk besloten Zafer en een groepje te vluchten ")
+
+		Tussenlijn()
+
+		print("Waar denk je dat ze heen gingen vluchten?")
+		print("A: Libanon")
+		print("B: Irak")
+		Tussenlijn()
+		Message7=input()
+		if Message7=="A":
+			Tussenlijn()
+			goed()
+			Tussenlijn()
+			lijn()
+			print("Hij ging met een groepje door Syrie naar Libanon")
+			print("dat landschap daar was erg rotsachtig dus ze moesten")
+			print("veel door de bergen lopen.")
+			Tussenlijn()
+			print("Waar is hij heen gevlucht?")
+			print("A: De zee op")
+			print("B: Turkije")
+			Tussenlijn()
+
+			Message8=input()
+			if Message8=="B":
+				Tussenlijn()
+				goed()
+				Tussenlijn()
+				lijn()
+				print("Zafer was met een groepje in Turkije gekomen")
+				print("daar ging hij in een hotel werken waar zijn baas")
+				print("zei dat hij kon blijven en zijn familie ook kon komen")
+				Tussenlijn()
+				print("Was zijn familie ook daarheen gegaan?")
+				Tussenlijn()
+				print("A:Ja")
+				print("B: Nee")
+				Tussenlijn()
+
+				Message9=input()
+				if Message9=="B":
+					Tussenlijn()
+					goed()
+					Tussenlijn()
+					lijn()
+					print("Zijn familie kon later niet komen, omdat Zafer")
+					print("zijn baas niet aan zijn belofde kon houden")
+					print("Zafer wou weg uit Turkije en ging dus met")
+					print("smokkelaars mee in een vrachtwagen vol met mensen.")
+					print("In die bus zaten veel mensen.")
+					Tussenlijn()
+					print("Hoeveel mensen gingen in zo'n vrachtwagen")
+					Tussenlijn()
+					print("A: 125")
+					print("B: 200")
+					print("C: 75")
+					Tussenlijn()
+					Message10=input()
+					if Message10=="B":
+						goed()
+						Tussenlijn()
+						lijn()
+						print("In de vrachtwagen gingen heel veel mensen ze stapelde")
+						print("zelfs op elkaar om er in te kunnen. Zafer kon amper")
+						print("bewegen, want als hij bewoog zat hij bij iemand anders")
+						print("op schoot. Uiteindelijk had Zafer door dat hij bij een")
+						print("klein vrouwtje op schoot zat en toen schaamde hij zich erg")
+						Tussenlijn()
+						print("Waar kwam de vrachtwagen aan?")
+						Tussenlijn()
+						print("A: Belgie")
+						print("B: Duitsland")
+						print("C: Nederland")
+						Tussenlijn()
+						Message11=input()
+						if Message11=="C":
+							goed()
+							Tussenlijn()
+							lijn()
+							print("Dit was het einde van het verhaal over Zafer zijn")
+							print("reis. Wil je meer weten begin dan opnieuw en kies")
+							print("een andere richting.")
+							lijn()
+						else:
+							fout()
+							lijn()
+					else:
+						fout()
+						lijn()
+				else:
+					fout()
+					lijn()
+			else:
+				fout()
+				lijn()
+
+		else:
+			fout()
+			lijn()
+	else:
+		Tussenlijn()
+		fout()
+		lijn()
+if Message2=="C":
+# 8 Stukken Tekst
+	Tussenlijn()
+	lijn()
+	print("Ik ga je nu wat vertellen over hoe het in Nederland.")
+	Tussenlijn()
+	print("Zafer was aangekomen in Nederland van een lange reis")
+	print("Hij kwam aan in een klein dorpje. Hij kreeg al snel hulp.")
+	print("er werdt tegen hem dat hij naar ...")
+	Tussenlijn()
+	print("Waar moest hij naar toe?")
+	Tussenlijn()
+	print("A: Amsterdam")
+	print("B: Rotterdam")
+	print("C: Den haag")
+	Tussenlijn()
+	Message12=input()
+	if Message12=="C":
+		goed()
+		lijn()
+		Tussenlijn()
+		print("Zafer kwam aan in Den haag waar hij een tijdelijke")
+		print("verblijfs vergunning kreeg en werdt door gestuurd")
+		print("naar een vluchtelingen opvang. zijn gezin mocht ... komen")
+		Tussenlijn()
+		print("Wat moet er op de lege plek")
+		print("A: Wel")
+		print("B: Niet")
+		Tussenlijn()
+		Message13=input()
+		if Message13=="B":
+			goed()
+			lijn()
+			Tussenlijn()
+			print("Zafer zijn gezin mocht nog niet komen. Dit kwam omdat")
+			print("Zafer nog geen verblijfsvergunning had alleen een tijdelijke")
+			print("vergunning. Zafer moest wachten op een brief waar hij dan")
+			print("toestemming in zou moeten krijgen")
+			Tussenlijn()
+			print("Wat ging hij in de tussen tijd doen")
+			Tussenlijn()
+			print("A: Boos worden omdat hij zolang moest wachten")
+			print("B: Niks en gewoon de tijd doden met de andere")
+			Tussenlijn()
+			Message14=input()
+			if Message14=="A":
+				goed()
+				lijn()
+				Tussenlijn()
+				print("Zafer moest erg lang wachten voor het antwoord van de brief.")
+				print("Hij kwam elke dag naar de begeleiders toe om te vragen")
+				print("of er al wat was binnen gekomen maar elkeer keer was het")
+				print("Nee Zafer er is niks binnen gekomen. Uiteindelijk")
+				print("was er een brief binnengekomen waarin stond dat hij")
+				print("mocht blijven.")
+				Tussenlijn()
+				print("Waarom duurde het zo lang voor dat hij antwoord kreeg?")
+				Tussenlijn()
+				print("A: De overheid moet alles controleren")
+				print("B: Er zijn nog veel andere vluchtelingen")
+				Tussenlijn()
+				Message15=input()
+				if Message15=="A":
+					goed()
+					print("Vraag B was ook goed")
+					lijn()
+					Tussenlijn()
+					print("Zafer had dus eindelijk zijn verblijfsvergunning")
+					print("gekregen. Nu mocht zijn gezin ook naar Nederland")
+					print("komen.")
+					Tussenlijn()
+					print("Waar kwam Zafer gezin aan in Nederland?")
+					Tussenlijn()
+					print("A: Schiphol")
+					print("B: Grens met Duitsland")
+					print("C: Grens met Belgie")
+					Tussenlijn()
+					Message16=input()
+					if Message16=="A":
+						goed()
+						lijn()
+						Tussenlijn()
+						print("Toen zijn gezin aan kwam op schiphol waren")
+						print("ze kinderen al erg gegroeit, want hij had ze")
+						print("een half jaar niet gezien. Nu iedereen in nederland")
+						print("is hebben ze een huis in Castricum gekocht.")
+						Tussenlijn()
+						print("Dit was het einde van het hoofdstuk in Nederland.")
+						print("Wil je meer weten begin dan opnieuw en kies")
+						print("een andere richting.")
+						lijn()
+					else:
+						fout()
+
+				elif Message15=="B":
+					goed()
+					print("Vraag A was ook goed")
+					lijn()
+					Tussenlijn()
+					print("Zafer had dus eindelijk zijn verblijfsvergunning")
+					print("gekregen. Nu mocht zijn gezin ook naar Nederland")
+					print("komen.")
+					Tussenlijn()
+					print("Waar kwam Zafer gezin aan in Nederland?")
+					Tussenlijn()
+					print("A: Schiphol")
+					print("B: Grens met Duitsland")
+					print("C: Grens met Belgie")
+					Tussenlijn()
+					Message16=input()
+					if Message16=="A":
+						goed()
+						lijn()
+						Tussenlijn()
+						print("Toen zijn gezin aan kwam op schiphol waren")
+						print("ze kinderen al erg gegroeit, want hij had ze")
+						print("een half jaar niet gezien. Nu iedereen in nederland")
+						print("is hebben ze een huis in Castricum gekocht.")
+						Tussenlijn()
+						print("Dit was het einde van het hoofdstuk in Nederland.")
+						print("Wil je meer weten begin dan opnieuw en kies")
+						print("een andere richting.")
+						lijn()
+
+					else:
+						fout()
+
+			else:
+				fout()
+				lijn()
+		else:
+			fout()
+			lijn()
+
+	else:
+		fout()
+		lijn()
 if Message2=="D":
-	print("")
+	Tussenlijn()
+	lijn()
 	print("Ik ga je nu vertellen over de toekomst van Zafer.")
-	print("")
+	Tussenlijn()
+	print("Nu eindelijk Zafer en zijn gezin weer samen zijn.")
+	print("Hebben ze besloten om in Nederland te gaan wonen")
+	print("ze hebben nu een huis in ... gekocht.")
+	Tussenlijn()
+	print("Wat moet er op de lege plek?")
+	Tussenlijn()
+	print("A: Bakkum")
+	print("B: Castricum")
+	print("C: Alkmaar")
+	Tussenlijn()
+	Message17=input()
+	if Message17=="B":
+		goed()
+		lijn()
+		Tussenlijn()
+		print("Zafer geeft nu voorlichtingen op scholen")
+		print("over zijn ervaringen. In de toekomst zou")
+		print("Zafer graag weer terug willen naar Syrie")
+		print("om het weer samen op te bouwen")
+		Tussenlijn()
+		print("dit was het hoofdstuk toekomst wil je meer")
+		print("weten begin dan weer opnieuw")
+		lijn()
+	else:
+		fout()
+		lijn()
